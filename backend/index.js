7145356e0f59650efd08934b30d2a106da3d9ae8
Archit-29 <archetsrivastav056@gmail.com>
2024-08-18 20:27:12 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import userRoute from "./route/user.route.js";
 import bookRoute from "./route/book.route.js";
 
@@ -24,6 +25,20 @@ console.log("error",error);
 
 app.use('/book',bookRoute);
 app.use('/user',userRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+   const dirPath = path.resolve();
+   
+   // Serve static files from the "dist" directory in the "Frontend" folder
+   app.use(express.static(path.join(dirPath, "Frontend", "dist")));
+   
+   // Serve the index.html file for all routes (SPA support)
+   app.get("*", (req, res) => {
+     res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
+   });
+ }
+ 
 
 
  app.listen(PORT,()=>{
